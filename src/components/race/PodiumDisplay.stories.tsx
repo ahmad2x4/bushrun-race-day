8fd45a1564baefe285b8f530d310f0c723957b37
Ahmad Atighechi <ara.mailbox@gmail.com>
2025-09-06 17:14:56 +1000

    fix storybook erros

--- src/components/race/PodiumDisplay.stories.tsx
+++ src/components/race/PodiumDisplay.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import PodiumDisplay from './PodiumDisplay'
 import type { Runner } from '../../types'
 
--- src/components/race/StaggeredStartQueue.stories.tsx
+++ src/components/race/StaggeredStartQueue.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import StaggeredStartQueue from './StaggeredStartQueue'
 import type { Race } from '../../types'
 
